f75ff0f730820785d6a82d6b9fd2784d4c3822d3
Tattsum <kurohari35@gmail.com>
2017-06-02 15:22:16 +0900

    追加

--- orelliy.rb
+++ orelliy.rb
@@ -5,6 +5,6 @@ open("http://www.oreilly.co.jp/catalog/") do |f|
   doc = Nokogiri::HTML.parse f.read
   prices = doc.css(".price").map{|p| p.text.tr(?,, '').to_i }
   amount = prices.inject(:+).to_s.gsub(/(\d)(?=(\d{3})+(?!\d))/, '\1,')
-
+  
   puts "合計 #{prices.size}冊 #{amount}円"
 end
