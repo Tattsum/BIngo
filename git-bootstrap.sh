b4350ed2d84f2ae88664c7251eb351bf65184544
Tattsum <kurohari35@gmail.com>
2017-08-22 17:54:28 +0900

    ちょっと変更

--- git-bootstrap.sh
+++ git-bootstrap.sh
@@ -5,6 +5,6 @@ git config --global core.editor 'vim -c "set fenc=utf-8"'
 git config --global color.diff auto
 git config --global color.status auto
 git config --global color.branch auto
-#git config --global push.default simple
+# git config --global push.default simple
 git config --global core.precomposeunicode true
 git config --global core.quotepath false
