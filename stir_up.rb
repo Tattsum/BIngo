dcfd0076a4373f5717cc320ee550f0d6888d8f08
Tattsum <kurohari35@gmail.com>
2017-09-06 09:57:59 +0900

    進捗はどうですか？

--- stir_up.rb
+++ stir_up.rb
@@ -1,18 +1,18 @@
 # 明日やろうは馬鹿野郎で煽るプログラム
 
-ary = ["明日", "やろうは", "馬鹿", "やろう"]
+ary = ["進捗", "は", "どうですか", "？"]
 num = Random.new
 str = String.new
 while true
   str += ary[num.rand(0..3)]
   print ary[num.rand(0..3)]
 
-  break if str =~ /明日やろうは馬鹿やろう/
+  break if str =~ /進捗はどうですか？/
 end
 
 puts ""
 puts "_人人人人人人人人人人人人人_"
-puts " >明日やろうは馬鹿やろう< "
+puts " >進捗はどうですか？< "
 puts "￣Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y￣"
 
 puts "#{str.length} 文字で煽られました．"
